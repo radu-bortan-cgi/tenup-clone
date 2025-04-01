bb86967e2a58f3b8921a1a5a2a010fd946504c3e
SSebigo <grezesfrancois@gmail.com>
2025-04-01 19:00:32 +0200

    Initial Commit

---
+++ tenup-cloneTests/tenup_cloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  tenup_cloneTests.swift
+//  tenup-cloneTests
+//
+//  Created by Margerite Fortuna on 01/04/2025.
+//
+
+import Testing
+@testable import tenup_clone
+
+struct tenup_cloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
